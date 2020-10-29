ed3fd4f45306c23fef7c3b14c90eb5c0bf9b8428
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2020-10-29 00:44:43 -0300

    Exercises 2-5

--- exercises/4.4/part-1/03-exercise.js
+++ exercises/4.4/part-1/03-exercise.js
@@ -2,8 +2,10 @@ let info = {
     personagem: "Margarida",
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim",
   };
 
-//   info.recorrente= "Sim",
 
-  console.log(info);
+for (let key in info) {
+    console.log(key)
+}
---
+++ exercises/4.4/part-1/04-exercise.js
@@ -0,0 +1,21 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim",
+  };
+
+
+for (let key in info) {
+    console.log(info[key])
+}
+
+let car = {
+    type: "Fiat",
+    model: "500",
+    color: "white",
+}
+
+for (let key in car) {
+    console.log(car[key])
+}
