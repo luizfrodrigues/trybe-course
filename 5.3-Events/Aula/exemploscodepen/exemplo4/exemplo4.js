71087e9587e245af09530bf23cbac02bf287ad51
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2020-11-12 15:23:41 -0300

    codepen4

---
+++ 5.3-Events/Aula/exemploscodepen/exemplo4/exemplo4.js
@@ -0,0 +1,11 @@
+const caixaTexto = document.querySelector('.caixa-texto');
+const caixaUm = document.querySelector('.caixa1');
+const caixaDois = document.querySelector('.caixa2');
+const botao1 = document.querySelector('.botao1');
+const botao2 = document.querySelector('.botao2');
+
+function trocaCor ( ){
+  let caixa = caixa1.style.backgroundColor = 'green';
+}
+
+botao1.addEventListener("mousemove", trocaCor(caixaUm));
