f0d38695d4b8938d82846732f6f8235a4dea1e73
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2020-11-26 00:23:42 -0300

    Exercicios de aula

---
+++ 7.2-JavaScript_ES6-Object/Aula/aula.js
@@ -0,0 +1,235 @@
+/* // const customer = {
+//   firstName: 'Roberto',
+//   age: 22,
+//   job: 'Teacher',
+// };
+
+const customer = {
+  firstName: 'Roberto',
+  lastName: 'Faria', // Propriedade adicionada.
+  age: 22,
+  job: 'Teacher',
+};
+
+const customer1 = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+console.log(customer1);
+
+customer1.lastName = 'Faria';
+console.log(customer1);
+
+const customer2 = {
+  firstName: 'Maria',
+  age: 23,
+  job: 'Medic',
+};
+
+console.log(customer2);
+customer2['lastName'] = 'Silva';
+console.log(customer2);
+
+customer1.age = 23;
+
+console.log(customer1) */
+/* 
+const customer = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+let newKey = 'lastName';
+const lastName = 'Ferreira';
+customer[newKey] = lastName;
+newKey = 'fullName';
+const fullName = `${customer.firstName} ${customer.lastName}`;
+customer[newKey] = fullName;
+console.log(customer); */
+
+/* const adress = {
+  endereco: 'Rua Marte',
+  numero: 429,
+  complemento: 'Torre 9, apto 125',
+  bairro: 'Jardim Tupanci',
+  CEP: '061414-000',
+  cidade: 'Barueri',
+  estado: 'São Paulo',
+}
+
+adress['pais'] = 'Brasil'; //adress.pais
+
+console.log(adress);
+ */
+/* 
+var arr = ['a', 'b', 'c'];
+console.log(Object.keys(arr)); // console: ['0', '1', '2'] */
+
+/* /* const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+ */
+
+/* const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const listSkills = (student) => {
+  const arrayOfSkills = Object.keys(student);
+  for(i in arrayOfSkills){
+    console.log(`${arrayOfSkills[i]}, Nível: ${student[arrayOfSkills[i]]
+    }`);
+  }
+};
+
+console.log('Estudante 1');
+listSkills(student1);
+
+console.log('Estudante 2');
+listSkills(student2);
+ */
+
+/* const aluno = {
+  matemática: 10,
+  português: 9,
+}
+
+console.log(Object.keys(aluno));
+console.log(Object.values(aluno));
+console.log(Object.entries(aluno)); */
+
+
+/* console.log(Object.values('foo'))
+
+const student = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkill: 'Ótimo',
+};
+
+const listSkillsWithFor = (student) => {
+  const skills = [];
+  for(skill in student) {
+    skills.push(student[skill]);
+  }
+
+  return skills;
+};
+
+const listSkillsWithValues = (student) => Object.values(student);
+
+// Sem Object.values
+console.log(listSkillsWithFor(student));
+
+// Com Object.values
+console.log(listSkillsWithValues(student)); */
+
+/* const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+// console.log(pairKeyValue);
+
+for(i in pairKeyValue) {
+  console.log('--------');
+  console.log('Pais:', pairKeyValue[i][0]);
+  console.log('Capital:', pairKeyValue[i][1]);
+}; */
+
+
+/* const person = {
+  name: 'Alberto',
+  lastName: 'Gomes',
+  age: 20,
+};
+
+const info = {
+  age: 23,
+  job: 'engenheiro',
+};
+
+const family = {
+  children: ['Maria', 'João'],
+  wife: 'Ana',
+};
+
+Object.assign(person, info, family)
+console.log(person)
+
+/* Output
+  { name: 'Alberto',
+  lastName: 'Gomes',
+  age: 23,
+  job: 'engenheiro',
+  child: [ 'Maria', 'João' ],
+  wife: 'Ana'
+  } */ 
+
+ /*  const person = {
+    name: 'Roberto',
+  };
+  
+  const lastName = {
+    lastName: 'Silva',
+  };
+  
+  const clone = Object.assign(person, lastName);
+  
+  console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+  console.log(person); // { name: 'Roberto', lastName: 'Silva' }
+
+clone.name = 'Maria';
+
+console.log('Mudando a propriedade name através do objeto clone')
+console.log(clone); // Output: { name: 'Maria', lastName: 'Silva' }
+console.log(person); // Output: { name: 'Maria', lastName: 'Silva' }
+console.log('--------------');
+
+person.lastName = 'Ferreira';
+
+console.log('Mudando a propriedade lastName através do objeto person');
+console.log(clone); // Output: { name: 'Maria', lastName: 'Ferreira' }
+console.log(person); // Output: { name: 'Maria', lastName: 'Ferreira' } */
+
+
+const person = {
+  name:'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const newPerson = Object.assign({},person,lastName);
+newPerson.name = 'Gilberto';
+console.log(newPerson);
+console.log(person);
